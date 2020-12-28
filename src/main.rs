52dfa57f32ef53461669348f0a3a94f604101c50
Emil Hernvall <emil@c0la.se>
2020-12-28 22:58:08 +0100

    feat: Render actual departure times for a station

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,52 @@ struct Opt {
     api_key: String,
 }
 
+mod trafiklab {
+    use chrono::prelude::*;
+    use serde_derive::Deserialize;
+
+    #[derive(Debug,Deserialize)]
+    #[serde(rename_all = "PascalCase")]
+    pub struct RealtimeDepartureInfo {
+        /// ex: tunnelbanans gröna linje
+        pub group_of_line: String,
+        /// ex: 1 min
+        pub display_time: String,
+        /// ex: METRO
+        pub transport_mode: String,
+        /// ex: Hagsätra
+        pub destination: String,
+        /// ex: 2
+        pub journey_direction: u32,
+        /// ex: Slussen
+        pub stop_area_name: String,
+        /// ex: 1011
+        pub stop_area_number: u32,
+        /// ex: 1012
+        pub stop_point_number: u32,
+        /// ex: 4
+        pub stop_point_designation: String,
+        /// ex: 2020-12-27T00:12:00
+        pub time_tabled_date_time: NaiveDateTime,
+        /// ex: 2020-12-27T00:12:00
+        pub expected_date_time: NaiveDateTime,
+        /// ex: 14759
+        pub journey_number: u32,
+    }
+
+    #[derive(Debug,Deserialize)]
+    #[serde(rename_all = "PascalCase")]
+    pub struct RealtimeDeparturesResponseData {
+        pub metros: Vec<RealtimeDepartureInfo>,
+    }
+
+    #[derive(Debug,Deserialize)]
+    #[serde(rename_all = "PascalCase")]
+    pub struct RealtimeDeparturesV4Response {
+        pub response_data: RealtimeDeparturesResponseData,
+    }
+}
+
 fn draw_pattern<S: Screen>(screen: &mut S) {
     for y in 0..screen.yres() {
         for x in 0..screen.xres() {
@@ -38,14 +84,59 @@ fn draw_pattern<S: Screen>(screen: &mut S) {
     }
 }
 
+fn read_test_data() -> Result<trafiklab::RealtimeDeparturesResponseData> {
+    let trafiklab::RealtimeDeparturesV4Response { response_data } =
+        serde_json::from_str(
+            &std::fs::read_to_string("./test/data/sl.json")?,
+        )?;
+
+    Ok(response_data)
+}
+
+fn fetch_from_server(api_key: &str, station_id: u32) -> Result<trafiklab::RealtimeDeparturesResponseData> {
+    let url = format!(
+        "https://api.sl.se/api2/realtimedeparturesV4.json?key={}&siteid={}&timewindow=60",
+        api_key,
+        station_id,
+    );
+    let response = dbg!(ureq::get(&url).call());
+    if !response.ok() {
+        panic!("error {}: {}", response.status(), response.into_string()?);
+    }
+
+    let trafiklab::RealtimeDeparturesV4Response { response_data } =
+        response.into_json_deserialize()?;
+
+    Ok(response_data)
+}
+
 fn main() -> Result<()> {
     let opt: Opt = argh::from_env();
+
+    let data = if opt.debug {
+        dbg!(read_test_data()?)
+    } else {
+        dbg!(fetch_from_server(&opt.api_key, opt.station_id)?)
+    };
 
     let mut screen = create_screen()?;
 
     loop {
-        draw_pattern(&mut screen);
-        screen.draw_text(10, 10, 32.0, "Testing with some smaller text");
+        // draw_pattern(&mut screen);
+        let mut y_offset = 0;
+        for departure in &data.metros {
+            screen.draw_text(
+                10,
+                y_offset,
+                32.0,
+                &format!("{} {}", departure.destination, departure.display_time),
+            );
+            y_offset += 32;
+
+            if y_offset + 32 > screen.yres() {
+                break;
+            }
+        }
         screen.flip();
         std::thread::sleep(std::time::Duration::from_millis(1000/60));
     }
